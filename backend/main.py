a24f588a0b085f9bc33f2a0a3e214afa2efd0d78
Gabomfim <gabomfim99@gmail.com>
2022-11-30 15:49:13 -0300

    fix backend init

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,63 @@
 from fastapi import FastAPI, UploadFile, File
-from backend import Location, ITEM_STATUS, set_item_status, set_item_location, get_item_location, get_not_scanned_itens, get_scanned_itens, get_moved_itens
 from enum import Enum
+import pandas as pd
+import pathlib
 import os
+from enum import Enum
 
+AREA_COLUMN = 'Área de Patrimônio'
+ID_COLUMN = 'Identificador'
+DEPRECATED_ID_COLUMN = 'PI Antigo'
+ORGANIZATION_DESCRIPTION_COLUMN = 'Desc. Órgão'
+BUILDING_COLUMN = 'Imóvel'
+N1_COLUMN = 'Local N1'
+N2_COLUMN = 'Local N2'
+N3_COLUMN = 'Local N3'
+N4_COLUMN = 'Local N4'
+N5_COLUMN = 'Local N5'
+
+ITEM_STATUS_COLUMN = 'item_status'
+
+COLUMNS = [
+    AREA_COLUMN,
+    ID_COLUMN,
+    DEPRECATED_ID_COLUMN,
+    ORGANIZATION_DESCRIPTION_COLUMN,
+    BUILDING_COLUMN,
+    N1_COLUMN,
+    N2_COLUMN,
+    N3_COLUMN,
+    N4_COLUMN,
+    N5_COLUMN,
+]
+
+class ITEM_STATUS(Enum):
+    NOT_SCANNED = 0
+    SCANNED = 1
+    SCANNED_AND_MOVED = 2
+
+class Location():
+
+    def __repr__(self):
+        return f'(area: {self.area},\
+        organization_description: {self.organization_description},\
+        building: {self.building},\
+        n1: {self.n1},\
+        n2: {self.n2},\
+        n3: {self.n3},\
+        n4: {self.n4},\
+        n5: {self.n5})\
+        '
+
+    def __init__(self, area: str, organization_description: str, building: str, n1: str, n2: str, n3: str, n4: str, n5: str):
+        self.area = area
+        self.organization_description = organization_description
+        self.building = building
+        self.n1 = n1
+        self.n2 = n2
+        self.n3 = n3
+        self.n4 = n4
+        self.n5 = n5
 
 app = FastAPI()
 #Rodar com 'uvicorn main:app --reload'
@@ -39,6 +94,10 @@ def upload(file: UploadFile = File(...)):
         contents = file.file.read()
         with open(file.filename, 'wb') as f:
             f.write(contents)
+        app.INVENTORY_FILE = pathlib.Path('inventory.xlsx')
+        app.inventory_table = pd.read_excel(app.INVENTORY_FILE, index_col=ID_COLUMN, usecols=COLUMNS)
+        #Creates a status column and initializes all of its cells with the NOT_SCANNED status value
+        app.inventory_table[app.ITEM_STATUS_COLUMN] = ITEM_STATUS.NOT_SCANNED.valu
     except Exception:
         return {"message": "There was an error uploading the file"}
     finally:
@@ -62,3 +121,30 @@ def unload_database():
 @app.get("/upload-check")
 def upload_check():
     return {"was-file-uploaded": app.was_file_uploaded}
+
+def set_item_status(item_id, status: ITEM_STATUS):
+    app.inventory_table.loc[item_id, ITEM_STATUS_COLUMN] = status.value
+    return status
+
+def set_item_location(item_id, location: Location):
+    app.inventory_table.loc[item_id, AREA_COLUMN] = location.area
+    app.inventory_table.loc[item_id, ORGANIZATION_DESCRIPTION_COLUMN] = location.organization_description
+    app.inventory_table.loc[item_id, BUILDING_COLUMN] = location.building
+    app.inventory_table.loc[item_id, N1_COLUMN] = location.n1
+    app.inventory_table.loc[item_id, N2_COLUMN] = location.n2
+    app.inventory_table.loc[item_id, N3_COLUMN] = location.n3
+    app.inventory_table.loc[item_id, N4_COLUMN] = location.n4
+    app.inventory_table.loc[item_id, N5_COLUMN] = location.n5
+    return location
+
+def get_item_location(inventory_table, item_id):
+    return inventory_table.loc[item_id].to_json(orient="index")
+
+def get_not_scanned_itens():
+    return app.inventory_table.loc[app.inventory_table[ITEM_STATUS_COLUMN] == ITEM_STATUS.NOT_SCANNED.value].to_json(orient="index")
+
+def get_scanned_itens():
+    return app.inventory_table.loc[app.inventory_table[ITEM_STATUS_COLUMN] == ITEM_STATUS.SCANNED.value].to_json(orient="index")
+
+def get_moved_itens():
+    return app.inventory_table.loc[app.inventory_table[ITEM_STATUS_COLUMN] == ITEM_STATUS.SCANNED_AND_MOVED.value].to_json(orient="index")
